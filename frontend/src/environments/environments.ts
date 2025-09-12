284dd09b4e44f919fe6c059055e01b5f57b2b52a
BUBERTO97 <roberto.amadio97@gmail.com>
2025-09-13 00:24:58 +0200

    init

---
+++ frontend/src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBgNLJKNFyERGfNszzH0y5u0s_yMSIeMJk",
+    authDomain: "fanta-tavolo-app.firebaseapp.com",
+    projectId: "fanta-tavolo-app",
+    storageBucket: "fanta-tavolo-app.firebasestorage.app",
+    messagingSenderId: "313897924265",
+    appId: "1:313897924265:web:e05f046370c3a0f46f5bb8",
+    measurementId: "G-RDDCT240BM"
+  }
+};
