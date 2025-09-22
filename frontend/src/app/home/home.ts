14702298dc36c3b5c4ab1efe2c748b9d26ff1f26
BUBERTO97 <roberto.amadio97@gmail.com>
2025-09-22 11:39:46 +0200

    fix home page typewriter

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, inject, ElementRef, ViewChild} from '@angular/core';
+import {Component, OnInit, inject, ElementRef, ViewChild, signal, effect} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { GameService } from '../game/game.service';
@@ -16,14 +16,59 @@ import { Pronostico } from '../game/game.models';
 export class HomeComponent implements OnInit {
   private readonly gameService = inject(GameService);
 
+  private readonly jokes = [
+    'Hello World! 👋 In questa applicazione sono nascosti diversi Easter Eggs... Un piccolo indizio? Controlla anche i display: "none" ;)',
+    "Sì, esatto, puoi cliccarmi ancora ed io cambierò per te...",
+    "Cliccami ancora...",
+    "Forza, cliccami ancora...",
+    "Vai, continua così🫣...",
+    "Ci sono 10 tipi di persone al mondo: quelle che capiscono il binario e quelle che non lo capiscono. Continua a cliccare...",
+    "Sì, esatto, puoi cliccarmi ancora e ancora...",
+    "Dai, ancora una volta...",
+    "Sorpresa! 🤯Jiale lavora al Tesake Sushi.🤯",
+    "In verità Diego (aka Siego) è Silvia con i baffi 🧔🏼‍♀️.",
+    "Amo premere f5, è così rinfrescante!",
+    'Qual’è la più grande bugia dell’universo? ”Ho letto e accetto i termini e le condizioni” (tranne per l\'Alby nazionale)'
+  ];
+
+  currentJoke = signal(this.jokes[0]);
+
+  displayedJoke = signal('');
+
+  private typingTimeout: any;
+  private readonly typingSpeed = 75;
+
 
   voters$!: Observable<Pronostico[]>;
 
 
   showEasterEggMessage = false;
 
-  private readonly EASTER_EGG_MESSAGE_DURATION = 10000; // 10 secondi
+  private readonly EASTER_EGG_MESSAGE_DURATION = 5000; // 10 secondi
 
+
+  constructor() {
+    effect(() => {
+      const jokeToDisplay = this.currentJoke(); // Read the signal to create a dependency
+      this.startTypewriter(jokeToDisplay);
+    });
+  }
+
+  private startTypewriter(text: string): void {
+    // Clear any previous, unfinished animation
+    clearTimeout(this.typingTimeout);
+    this.displayedJoke.set(''); // Reset the displayed text
+
+    let charIndex = 0;
+    const type = () => {
+      if (charIndex < text.length) {
+        this.displayedJoke.update(currentText => currentText + text.charAt(charIndex));
+        charIndex++;
+        this.typingTimeout = setTimeout(type, this.typingSpeed);
+      }
+    };
+    type(); // Start the typing
+  }
 
 
   ngOnInit() {
@@ -39,6 +84,11 @@ export class HomeComponent implements OnInit {
     setTimeout(() => {
       this.showEasterEggMessage = false;
     }, this.EASTER_EGG_MESSAGE_DURATION);
+  }
+
+  jokeClick(){
+    const randomIndex = Math.floor(Math.random() * this.jokes.length);
+    this.currentJoke.set(this.jokes[randomIndex]);
   }
 
 }
